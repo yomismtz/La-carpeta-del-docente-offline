2d37e5a5d9a041e444ed374aaeaa967d87d1c821
Yomi <yomismtz@gmail.com>
2026-09-11 06:38:43 -0600

    Bump polished tester build to 1.9.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.profecuaderno.offline"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.9.0"
+        versionCode = 15
+        versionName = "1.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
